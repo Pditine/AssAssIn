2d361da8e4a960e89faace36506fe0f26247f887
Hmxs <18321938171@163.com>
2024-03-11 22:54:34 +0800

    Update Some Test Data

---
+++ Assets/Hmxs/Scripts/PlayerInputManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Hmxs.Scripts
+{
+    public class PlayerInputManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Hmxs/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Hmxs.Scripts
+{
+    public class Test : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+
+        private void Start()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _playerInput.actions["Act"].performed += _ =>
+            {
+                Debug.Log("Act" + name);
+            };
+        }
+
+
+    }
+}
