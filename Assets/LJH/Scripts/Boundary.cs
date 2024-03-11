5cf10d2c19477e35f14cfc609f13fc0050c03c1b
Pditine <hanfe666@outlook.com>
2024-03-11 21:22:06 +0800

    Initial commit

---
+++ Assets/LJH/Scripts/Boundary.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LJH.Scripts
+{
+    public class Boundary : MonoBehaviour
+    {
+        //[SerializeField] private float offset;
+        [SerializeField] private Vector2 normalDirection;
+        //[SerializeField] private float force;
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                var thePlayer = other.gameObject.GetComponent<PlayerController>();
+                var originDirection = thePlayer.Direction;
+                Vector2 Out_Direction = Vector2.Reflect(originDirection,normalDirection);
+
+                thePlayer.Direction = Out_Direction;
+                
+                //other.transform.position += (Vector3)normalDirection*offset;
+            }
+            
+        }
+    }
+}
---
+++ Assets/LJH/Scripts/PlayerAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LJH.Scripts
+{
+    public class PlayerAction : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        private PlayerController _thePlayer;
+        private void Start()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _thePlayer = FindObjectsOfType<PlayerController>().FirstOrDefault(p=>p.id == _playerInput.playerIndex);
+        }
+
+        public void ChangeDirection(InputAction.CallbackContext ctx)
+        {
+            if (!_thePlayer) return;
+            _thePlayer.ChangeDirection(ctx);
+        }
+
+        public void Launch(InputAction.CallbackContext ctx)
+        {
+            if (!_thePlayer) return;
+            _thePlayer.Launch(ctx);
+        }
+        
+    }
+}
