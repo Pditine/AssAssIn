43c243e760f5418e407e789450b2494ca87ab098
Hmxs <18321938171@163.com>
2024-03-12 15:38:27 +0800

    Update PlayerControlManager.cs

---
+++ Assets/Hmxs/Scripts/PlayerControlManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Hmxs.Scripts
+{
+    public class PlayerControlManager : MonoBehaviour
+    {
+        private PlayerInputManager _playerInputManager;
+
+        private void Start()
+        {
+            _playerInputManager = GetComponent<PlayerInputManager>();
+            _playerInputManager.onPlayerJoined += OnPlayerJoined;
+            _playerInputManager.onPlayerLeft += OnPlayerLeft;
+        }
+
+        private void OnPlayerJoined(PlayerInput playerInput)
+        {
+            Debug.Log("Player Join" + playerInput.devices[0].name);
+        }
+
+        private void OnPlayerLeft(PlayerInput playerInput)
+        {
+            Debug.Log("Player Left" + playerInput.devices[0].name);
+        }
+    }
+}
--- Assets/Hmxs/Scripts/PlayerInputManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Hmxs.Scripts
-{
-    public class PlayerInputManager : MonoBehaviour
-    {
-        
-    }
-}
